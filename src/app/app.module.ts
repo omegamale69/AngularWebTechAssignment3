0cf2ede92dd27996f463acb046363a7d434f53b2
DavidDannenberg <d.a.dannenberg@gmail.com>
2022-05-09 15:55:13 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { CommunityComponent } from './community/community.component';
+import { PaginationComponent } from './pagination/pagination.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    CommunityComponent,
+    PaginationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
